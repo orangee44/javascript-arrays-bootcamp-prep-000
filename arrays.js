ab04935e879b6c2abba82ff403ec3e07fdabe69c
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 14:02:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,9 +15,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  
+  return array.push(element);
 }
