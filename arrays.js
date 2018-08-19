b9876391d89c6d8fc686c228edfd54f684f119ea
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 14:13:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  array = array.slice(1);
   return array;
 }
