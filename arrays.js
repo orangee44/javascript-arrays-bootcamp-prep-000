49fe4dcbbb2f1e0bcef3f7d51d9e4fdb68c2cbac
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 14:02:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,7 @@ var chocolateBars = [
   ]
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, 
-  ...array];
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
