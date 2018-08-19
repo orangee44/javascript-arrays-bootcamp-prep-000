aab3ab6dfa15b2d7ecfdba4ab27f9ab8c53d9bde
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 14:00:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,6 @@ var chocolateBars = [
   'skittles'
   ]
 
-
 function addElementToBeginningOfArray(array, element) {
   return [element, 
   ...array];
@@ -15,4 +14,6 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
 }
 
+function addElementToEndOfArray(array, element) {
   
+}
